7bf26834f90af106a5255ef3ee287a0c69a35ae5
Susa1ev <susalytt@gmail.com>
2021-11-30 17:43:58 +0300

    Добавьте файлы проекта.

---
+++ prac2.4(c)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace prac2._4_c_
+{
+    class prac
+    {
+        public prac()
+        {
+            Main();
+        }
+        void Main()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 4 - i; j >= 0; j--)
+                {
+                    Console.Write(j + "\t");
+                }
+                Console.Write("\v");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
